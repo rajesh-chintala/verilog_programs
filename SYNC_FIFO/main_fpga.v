22493187e3d8597da90aa1b8a2d0cc926c8f829a
rajesh-chintala <n210156@rguktn.ac.in>
2026-04-01 08:54:31 +0530

    Create main_fpga.v

---
+++ SYNC_FIFO/main_fpga.v
@@ -0,0 +1,2 @@
+// Added output led logic to tell the data in specific location
+// Clock will be generated from the push buttons not the default 100MHz clock, to prevent mishapps or unknowns read or write operation, unintended things.
